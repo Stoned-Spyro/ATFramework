6d7ebc74b98655507f7b4cd4f51c156b4221c179
Oleksiy Slavych <alexseeko15@gmail.com>
2022-06-25 00:58:25 +0300

    JMeter

--- src/main/java/constants/APIConstants.java
+++ src/main/java/constants/APIConstants.java
@@ -1,7 +1,10 @@
 package constants;
 
 public class APIConstants {
-        public static String URI = "https://aquaqualityweb.azurewebsites.net";
+    //   public static String URI = "https://aquaqualityweb.azurewebsites.net";
+     //   public static String URI = "http://localhost:5000";
+        public static String URI = "https://localhost:5001";
         public static String PATH = "/api";
+
 
 }
